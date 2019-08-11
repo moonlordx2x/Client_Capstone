970c0218084f2fdf325198c5f025f22fa2017b98
moonlordx2x <moonlordx2x>
2019-08-12 00:31:21 +0800

    Commit

--- Assets/Script/Player/Player_Controller.cs
+++ Assets/Script/Player/Player_Controller.cs
@@ -5,16 +5,18 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player_Controller : MonoBehaviour {
 
-    private float speed = 40f;
+    private float speed = 50f;
     private float movement = 0f;
     public Animator animate;
     public Rigidbody2D Rigidbody2D;
+    public GameObject Game_over;
 
 
     // Use this for initialization
     void Start() {
         Rigidbody2D = GetComponent<Rigidbody2D>();
         animate = GetComponent<Animator>();
+        Game_over.SetActive(false);
 
     }
 
@@ -22,8 +24,6 @@ public class Player_Controller : MonoBehaviour {
     void Update() {
         movement = CrossPlatformInputManager.GetAxis("Horizontal");
         Rigidbody2D.velocity = new Vector2(speed * movement, Rigidbody2D.velocity.y);
-
-        Debug.Log(movement);
 
         if (movement < 0 || Input.GetKey("a")) { transform.localScale = new Vector2(-1f, 1f);
             animate.SetTrigger("Running");
@@ -34,6 +34,16 @@ public class Player_Controller : MonoBehaviour {
         else {
             Rigidbody2D.velocity = new Vector2(0, Rigidbody2D.velocity.y);
             animate.SetTrigger("Idle");
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Falling_Object")
+        {
+            Game_over.SetActive(true);
+            Time.timeScale = 0f;
         }
     }
 }
